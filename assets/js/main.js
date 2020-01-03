c74f6d1e51cc8096702440d7109dafe777dbb4da
Nicholas Murray <nicholasmurray@wustl.edu>
2020-01-03 12:33:53 -0500

    changed image path for prod

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,8 @@ $(document).ready(function(){
             let $video = $(video);
             if (window.chrome) videoURL = videoURL.replace(".mp4", ".webm");
             // $video.attr('src', `assets/${videoURL}`);
-            $video.attr('src', `http://127.0.0.1:4000/assets/images/${videoURL}`);
+            // $video.attr('src', `http://127.0.0.1:4000/assets/images/${videoURL}`);
+            $video.attr('src', `assets/images/${videoURL}`);
             $video.attr('muted', true);
             $video.attr('type', 'video/mp4'); // type="video/mp4"
             $video.attr('autoplay', true);
